92e557f694a5cbc64be13f42c7cd9a5bad8ce2ab
Heigz <UchihaItachi@ciencias.unam.mx>
2024-02-11 22:20:44 -0600

    add: command(matate,rangos), respuesta(que hora es, para que sirves)

--- Bot.py
+++ Bot.py
@@ -2,52 +2,74 @@ import discord
 import respuestas
 from discord.ext import commands
 
+
 # async function to send message
-async def send_message(message,user_message,is_private):
+async def send_message(message, user_message, is_private):
     respoonse = respuestas.get_response(user_message)
-    await message.author.send(respoonse) if is_private else await message.channel.send(respoonse)
+    (
+        await message.author.send(respoonse)
+        if is_private
+        else await message.channel.send(respoonse)
+    )
 
 
 def run_discord_bot():
-    TOKEN = '<EL TOKEN VA A AQUI>'
+    TOKEN = "MTIwNDg1NDU5ODAyODc1OTExMg.GFgCot.1e2edMXzqzEgxdea44l8b9qp_6v292BY_GHeNo"
     intents = discord.Intents.default()
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix='!', intents=intents)
+    bot = commands.Bot(command_prefix="!", intents=intents)
 
     @bot.command()
     async def ping(ctx):
-        await ctx.send("pong")   
+        await ctx.send("pong")
 
     @bot.command()
     async def downloadLol(ctx):
-        await ctx.send('¿Quieres descargar LOL? aquí tienes:\nhttps://signup.leagueoflegends.com/es-mx/signup/redownload')
+        await ctx.send(
+            "¿Quieres descargar LOL? aquí tienes:\nhttps://signup.leagueoflegends.com/es-mx/signup/redownload"
+        )
+
+    @bot.command()
+    async def matate(ctx):
+        """Te contesta descaradamente"""
+        await ctx.send("Ni sabes jugar bien hazlo tu")
+
+    @bot.command()
+    async def rangos(ctx):
+        """ "Muestra los rangos disponibles de lol"""
+        await ctx.send(
+            "-Hierro\n-Bronce\n-Plata\n-Oro\n-Platino\n-Esmeralda\n-Diamante\n-Maestro\n-Gran Maestro\n-Retador"
+        )
 
     @bot.event
     async def on_ready():
-        print(f'{bot.user} bienvenido a la ruta del invocador')
+        print(f"{bot.user} bienvenido a la ruta del invocador")
 
     @bot.event
     async def on_message(message):
         if message.author == bot.user:
             return
-    
+
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
 
-        print(f'{username} said: {user_message} in {channel}')
+        print(f"{username} said: {user_message} in {channel}")
 
-        if isinstance(message.channel, discord.TextChannel) and message.channel.name == "bot-testing":
-            if user_message.startswith('!priv'):
+        if (
+            isinstance(message.channel, discord.TextChannel)
+            and message.channel.name == "bot-testing"
+        ):
+            if user_message.startswith("!priv"):
                 user_message = user_message[5:]  # Eliminar el comando "!priv"
                 await send_message(message, user_message, is_private=True)
             else:
-                if not user_message.startswith('!'):
+                if not user_message.startswith("!"):
                     await send_message(message, user_message, is_private=False)
-        
+
         await bot.process_commands(message)
-    
+
     bot.run(TOKEN)
 
     try:
@@ -56,8 +78,5 @@ def run_discord_bot():
         print("Proceso interrumpido. Saliendo...")
         bot.logout()
 
+
 run_discord_bot()
-
-    
-
-    
